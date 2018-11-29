2e0e43444a323cd369aa6bfc6ee42106f2c1fe38
Rohan <rohan1agg@gmail.com>
2018-11-29 13:48:43 +0530

    V1.0.2 Transaction for placing order

--- src/resources/order.js
+++ src/resources/order.js
@@ -1,9 +1,9 @@
 // Dependencies
 import express from 'express'
-import getConnection from './dbconnection'
+import getConPool from './dbconnection'
 
 const order = express.Router()
-const conPool = getConnection()
+const conPool = getConPool()
 
 order.get('/all', (req, res) => {
   var sql = 'SELECT * FROM user_orders'
@@ -78,38 +78,65 @@ order.get('/id', (req, res) => {
 })
 
 order.post('/', (req, res) => {
-  var orderAmount, orderID
-  var sql = 'SELECT SUM(price) AS amount FROM products WHERE product_id IN (?)'
-  var values = req.body.items.map(x => x.productId)
-  conPool.query(sql, [values], (err, result, fields)=>{
-    if (err) {
-      res.sendStatus(500)
-      res.end()
-      console.log(err)
-      return
-    }
-    orderAmount = result[0].amount
-    sql = 'INSERT INTO user_orders (user_id, order_amount, order_status) VALUES ?'
-    values = [[req.body.userId, orderAmount, req.body.orderStatus]]
-    conPool.query(sql, [values], (err, result, fields) => {
-      if (err) {
+  conPool.getConnection((err, con)=>{
+    con.beginTransaction(err => {
+      if(err){
         res.sendStatus(500)
         res.end()
         console.log(err)
         return
       }
-      orderID = result.insertId
-      var items = req.body.items.map(x => [orderID, x.productId, x.dealerId, x.quantity])
-      sql = 'INSERT INTO order_contents (order_id, product_id, dealer_id, quantity) VALUES ?'
-      conPool.query(sql, [items], (err, result, fields)=>{
-        if(err){
-          res.sendStatus(500)
-          res.end()
-          console.log(err)
-          return
-        }
-        console.log('Order Contents inserted rows' + result.affectedRows)
-        res.send('Your order Placed has been successfully placed!\n Order ID: ' + orderID + ', \nTotal Order Amount = ' + orderAmount)
+      var orderAmount, orderID
+      var sql = 'SELECT SUM(price) AS amount FROM products WHERE product_id IN (?)'
+      var values = req.body.items.map(x => x.productId)
+      var promQ = new Promise((resolve, reject) => {
+        con.query(sql, [values], (err, result, fields)=>{
+          if (err) {
+            reject(err)
+            console.log(err)
+            return
+          }
+          orderAmount = result[0].amount
+          resolve(orderAmount)
+        })
+      })
+      promQ.then(()=>{
+        sql = 'INSERT INTO user_orders (user_id, order_amount, order_status) VALUES ?'
+        values = [[req.body.userId, orderAmount, req.body.orderStatus]]
+        var promQ2 = new Promise((resolve, reject) => {
+          con.query(sql, [values], (err, result, fields) => {
+            if (err) {
+              reject(err)
+              console.log(err)
+              return
+            }
+            orderID = result.insertId
+            resolve(result.insertId)
+          })
+        })
+        promQ2.then((ID)=>{
+          var items = req.body.items.map(x => [ID, x.productId, x.dealerId, x.quantity])
+          sql = 'INSERT INTO order_contents (order_id, product_id, dealer_id, quantity) VALUES ?'
+          con.query(sql, [items], (err, result, fields)=>{
+            if(err){
+              res.sendStatus(500)
+              res.end()
+              con.rollback(()=>{console.log(err)})
+              return
+            }
+            con.commit(err=>{
+              if(err){
+                res.sendStatus(500)
+                res.end()
+                con.rollback(()=>{console.log(err)})
+                return
+              }
+              console.log('Success')
+              console.log('Order Contents inserted rows' + result.affectedRows)
+              res.send('Your order Placed has been successfully placed!\n Order ID: ' + orderID + ', \nTotal Order Amount = ' + orderAmount)
+            })
+          })
+        })
       })
     })
   })
